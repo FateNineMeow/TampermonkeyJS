a5f0d15d4b5bd7df50919cf1763ddd56fb3b91ee
敲键听响 <otaker@126.com>
2022-12-19 17:21:28 +0800

    增加对 learn.microsoft.com 的支持

--- src/MicrosoftDoc.js
+++ src/MicrosoftDoc.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://docs.microsoft.com/*
+// @match        https://learn.microsoft.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @grant        none
 // ==/UserScript==
